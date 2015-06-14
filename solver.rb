bbcc5fb00ecce46dfe9e05774406e07f30f4617a
compwron <compiledwrong@gmail.com>
2015-06-14 00:35:05 -0700

    messing about

--- solver.rb
+++ solver.rb
@@ -1,5 +1,6 @@
 require 'pry'
 class Solver
+	# http://www.policyalmanac.org/games/aStarTutorial.htm
 	START_POSITION = 'S'
 	END_POSITION = 'E'
 	WALL = '*'
@@ -7,12 +8,17 @@ class Solver
 	def initialize(filepath)
 		@maze = File.read(filepath).split("\n")
 		@current_position = start_position
+		@has_visited = [clone(@current_position)]
 	end
 
 	def display
-		temp_maze = Marshal.load(Marshal.dump(@maze)) # deep copy for display
+		temp_maze = clone(@maze)
 		temp_maze[@current_position.last][@current_position.first] = 'M'
 		temp_maze
+	end
+
+	def clone(thing)
+		Marshal.load(Marshal.dump(thing)) # deep copy for display
 	end
 
 	def start_position
@@ -37,8 +43,16 @@ class Solver
 		[xy.first - 1, xy.last]
 	end
 
+	def right(xy)
+		[xy.first + 1, xy.last]
+	end
+
 	def up(xy)
 		[xy.first, xy.last - 1]
+	end
+
+	def down(xy)
+		[xy.first, xy.last + 1]
 	end
 
 	def move_dir(direction)
@@ -47,18 +61,37 @@ class Solver
 			@current_position = left(@current_position)
 		when :up
 			@current_position = up(@current_position)
+		when :right
+			@current_position = right(@current_position)
+		when :down
+			@current_position = down(@current_position)
 		end
+		@has_visited << clone(@current_position)
+	end
+
+	def shouldnt_go(xy)
+		is_wall(char_at(xy)) || @has_visited.include?(xy)
 	end
 
 	def move
-		unless is_wall(char_at(left(@current_position)))
+		unless shouldnt_go(left(@current_position))
 			move_dir(:left) 
 			return display 
 		end
 
-		unless is_wall(char_at(up(@current_position)))
+		unless shouldnt_go(up(@current_position))
 			move_dir(:up) 
 			return display 
+		end
+
+		unless shouldnt_go(right(@current_position))
+			move_dir(:right)
+			return display
+		end
+
+		unless shouldnt_go(down(@current_position))
+			move_dir(:down)
+			return display
 		end
 		'fooo'
 		# display
@@ -68,7 +101,7 @@ end
 s = Solver.new('maze.txt')
 # puts s.display
 # s.start_position
-(0..3).each do |i|
+(0..40).each do |i|
 	puts i
 	puts s.move
 end
